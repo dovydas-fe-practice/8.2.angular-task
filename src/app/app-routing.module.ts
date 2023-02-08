a0bef4c81ae28f040737d10b8496e691756c1c83
Dovydas Miknys <Dovydas.Miknys@itechart-group.com>
2023-02-08 15:37:33 +0200

    any path resolves to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'details/:id', component: HeroDetailComponent },
+  { path: '**', component: DashboardComponent },
 ];
 
 @NgModule({
